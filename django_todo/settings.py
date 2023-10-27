ca5e7cf13a199d940f667533f93487370541e209
app-wizard <>
2023-10-27 23:06:13 +0000

    fix

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+if os.path.exists("env.py"):
+    import env
 
 from pathlib import Path
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!1jdz3##^bopm2u&t%nuhj6nq6y(1ev=(l2=hb%-)-lmdcvcl5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
